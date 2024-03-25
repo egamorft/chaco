857854ab2aa5202cd77ec197c6ae420bde458c55
datnguyen2001 <nguyendatdz01@gmail.com>
2024-03-25 08:27:46 +0700

    home

---
+++ wp-content/plugins/chacos/view/layout/footer.php
@@ -0,0 +1,83 @@
+<div class="box-big-footer">
+   <div class="box-footer">
+      <div class="d-flex justify-content-between align-center flex-wrap">
+         <div class="box-help">
+            <p class="text-help">NEED HELP?</p>
+            <div class="w-100 d-flex align-items-center">
+               <div class="box-item-help">
+                  <img src="<?= WIDGET_URL_CHACOS ?>/asset/img/call.png" class="icon-contanct">
+                  <span>(888) 211 - 1908</span>
+               </div>
+               <div  class="box-item-help">
+                  <img src="<?= WIDGET_URL_CHACOS ?>/asset/img/chat.png" class="icon-contanct">
+                  <span>Chat Now</span>
+               </div>
+            </div>
+         </div>
+        
+         <div class="box-join">
+            <p class="text-help">JOIN THE #CHACONATION</p>
+            <div class="d-flex align-items-center justify-content-between">
+               <img src="<?= WIDGET_URL_CHACOS ?>/asset/img/instagram.png" class="icon-contanct">
+               <img src="<?= WIDGET_URL_CHACOS ?>/asset/img/tik-tok.png" class="icon-contanct">
+               <img src="<?= WIDGET_URL_CHACOS ?>/asset/img/pinterest.png" class="icon-contanct">
+               <img src="<?= WIDGET_URL_CHACOS ?>/asset/img/youtube.png" class="icon-contanct">
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="box-footer-bottom">
+         <div class="d-flex flex-column">
+            <p class="title-footer-link">POPULAR LINKS</p>
+            <a href="" class="name-footer-link">Best Sellers</a>
+            <a href="" class="name-footer-link">New Arrivals</a>
+            <a href="" class="name-footer-link">Slides</a>
+            <a href="" class="name-footer-link">Sandals</a>
+            <a href="" class="name-footer-link">Custom</a>
+            <a href="" class="name-footer-link">Shoes</a>
+            <a href="" class="name-footer-link">Boots</a>
+            <a href="" class="name-footer-link">Text an Expert</a>
+         </div>
+         <div class="d-flex flex-column">
+            <p class="title-footer-link">POPULAR LINKS</p>
+            <a href="" class="name-footer-link">888-211-1908</a>
+            <a href="" class="name-footer-link">Contact Us</a>
+            <a href="" class="name-footer-link">Shipping Information</a>
+            <a href="" class="name-footer-link">Returns & Exchanges</a>
+            <a href="" class="name-footer-link">Warranty</a>
+            <a href="" class="name-footer-link">FAQ</a>
+         </div>
+         <div class="d-flex flex-column">
+            <p class="title-footer-link">POPULAR LINKS</p>
+            <a href="" class="name-footer-link">Order Status</a>
+            <a href="" class="name-footer-link">Shopping Cart</a>
+            <a href="" class="name-footer-link">Sign In</a>
+            <a href="" class="name-footer-link">Register</a>
+            <a href="" class="name-footer-link">Email Unsubscribe</a>
+         </div>
+         <div class="d-flex flex-column">
+            <p class="title-footer-link">POPULAR LINKS</p>
+            <a href="" class="name-footer-link">Sustainability</a>
+            <a href="" class="name-footer-link">Find A Store</a>
+            <a href="" class="name-footer-link">Size Chart</a>
+            <a href="" class="name-footer-link">Sandal Adjustment</a>
+            <a href="" class="name-footer-link">Gift Cards</a>
+            <a href="" class="name-footer-link">Careers</a>
+            <a href="" class="name-footer-link">Blog</a>
+            <a href="" class="name-footer-link">Email Preferences</a>
+            <a href="" class="name-footer-link">Pro Deal</a>
+         </div>
+         <div class="d-flex flex-column">
+            <p class="title-footer-link">POPULAR LINKS</p>
+            <p class="title-small-footer">Get 20% off your first order! Plus, be the first to know about new arrivals, stories, and events</p>
+            <div>
+               <input type="text" class="input-footer-sign" placeholder="Email Address">
+               <button class="btn-sign-footer">Sign up</button>
+            </div>
+            <p class="title-small-footer">This site is protected by reCAPTCHA and the Policy and Terms of Service apply.</p>
+         </div>
+      </div>
+     
+   </div>
+</div>
